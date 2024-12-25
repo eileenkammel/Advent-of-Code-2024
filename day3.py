b7a86ffca743350d09804bed3b66b49edfa40a21
Eileen <eniedenfuehr@gmail.com>
2024-12-25 13:19:56 +0100

    solved day 3 part 2

--- day3.py
+++ day3.py
@@ -5,8 +5,14 @@ from day1 import read_file
 def parse_data(data):
     valid = []
     pattern = re.compile(r"mul\(\d{1,3},\d{1,3}\)")
-    for line in data:
-        valid.extend(pattern.findall(line))
+    valid.extend(pattern.findall(data))
+    return valid
+
+
+def reduce_data(data):
+    pattern = re.compile(r"do\(\)(.+?)don't\(\)")
+    valid = []
+    valid.extend(pattern.findall(data))
     return valid
 
 
@@ -26,9 +32,21 @@ def calc_endresult(data):
 
 def main():
     data = read_file("day3_input.txt")
+    data = "".join(data)
     valid = parse_data(data)
+    return calc_endresult(valid)
+
+
+def main_part_2():
+    data = read_file("day3_input.txt")
+    data = ["do()"] + data + ["don't()"]
+    data = "".join(data)
+    valid = reduce_data(data)
+    valid = "".join(valid)
+    valid = parse_data(valid)
     return calc_endresult(valid)
 
 
 if __name__ == "__main__":
     print(main())
+    print(main_part_2())
